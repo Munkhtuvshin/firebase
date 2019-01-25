f31efcc26e91444cc8666b3fee8431dacfd2152f
pc <monhood34@gmail.com>
2019-01-24 21:23:05 -0600

    init firebase

--- App.js
+++ App.js
@@ -7,8 +7,18 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text,TextInput,Button,FlatList, View} from 'react-native';
+import * as firebase from 'firebase';
 
+const config = {
+  apiKey: "AIzaSyAByWagzzVchHysS_kQ9YVN2GbNVvnynkw",
+  authDomain: "fir-examle-c2e5c.firebaseapp.com",
+  databaseURL: "https://fir-examle-c2e5c.firebaseio.com",
+  projectId: "fir-examle-c2e5c",
+  storageBucket: "fir-examle-c2e5c.appspot.com",
+  messagingSenderId: "635525383647"
+};
+firebase.initializeApp(config);
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -18,32 +28,106 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      message: '',
+      messages: []
+    }
+
+    this.addItem = this.addItem.bind(this);
+  }
+  componentDidMount() {
+    firebase
+      .database()
+      .ref()
+      .child("messages")
+      .once("value", snapshot => {
+        const data = snapshot.val()
+        if (snapshot.val()) {
+          const initMessages = [];
+          Object
+            .keys(data)
+            .forEach(message => initMessages.push(data[message]));
+          this.setState({
+            messages: initMessages
+          })
+        }
+      });
+
+    firebase
+      .database()
+      .ref()
+      .child("messages")
+      .on("child_added", snapshot => {
+        const data = snapshot.val();
+        if (data) {
+          this.setState(prevState => ({
+            messages: [data, ...prevState.messages]
+          }))
+        }
+      })
+
+  }
+
+  addItem () {
+    if (!this.state.message) return;
+
+    const newMessage = firebase.database().ref()
+                          .child("messages")
+                          .push();
+    newMessage.set(this.state.message, () => this.setState({message: ''}))
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View style={styles.msgBox}>
+          <TextInput placeholder='Enter your message'
+            value={this.state.message}
+            onChangeText={(text) => this.setState({message: text})}
+            style={styles.txtInput}/>
+          <Button title='Send' onPress={this.addItem}/>
+        </View>
+        <FlatList data={this.state.messages}
+          renderItem={
+            ({item}) => 
+            <View style={styles.listItemContainer}>
+              <Text style={styles.listItem}>
+                {item}
+              </Text>
+            </View>
+          }
+          />
       </View>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#eee',
+    marginTop: 90
   },
-  welcome: {
+  msgBox: {
+    flexDirection: 'row',
+    padding: 20,
+    backgroundColor: '#fff'
+  },
+  txtInput: {
+    flex: 1
+  },
+  listItemContainer: {
+    backgroundColor: '#fff',
+    margin: 5,
+    borderRadius: 5
+  },
+  listItem: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    padding: 10
+  }
 });
